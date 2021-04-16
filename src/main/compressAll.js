5b86962849e2d1e284428dccc36500bbea128af1
Alexander Ilyin <alexander.ilyin3@gmail.com>
2021-04-17 00:35:25 +0300

    added converting to jpg format

--- src/main/compressAll.js
+++ src/main/compressAll.js
@@ -4,25 +4,28 @@ module.exports = async (imgArray) => {
   return new Promise(async(rs,rj) => {
     const response = []
     for await ( const imgItem of imgArray ) {
-      const { name, path } = imgItem
+      const name = imgItem.name.match(/(.*)\./)[1]
+      console.log({name})
+      const { path } = imgItem
       await new Promise((rs,rj) => {
         sharp(path)
         .metadata()
         .then((metadata) => {
-          console.log({metadata})
+          // console.log({metadata})
           const responseObject = {
             oldWidth: metadata.width,
             oldHeight: metadata.height
           }
 
-          
-          
+
+
           if ( metadata && metadata.width > 1200 ) {
 
              sharp(path)
               .resize(1200)
-              .png()
-              .toFile('./processedPictures/' + name, (err, picinfo) => {
+              // .toFormat('png')
+              .toFormat('jpg')
+              .toFile('./processedPictures/' + name + '.jpg', (err, picinfo) => {
                 // console.log({err})
                 console.log('processing PIC')
                 // console.log({picinfo})
