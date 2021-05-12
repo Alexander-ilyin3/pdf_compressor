4a0438541fb308d0fc5e580c304bdf9744062a3b
Alexander Ilyin <alexander.ilyin3@gmail.com>
2021-05-13 02:24:35 +0300

    font generator fix try

---
+++ src/frontend/components/textTemplates/addTemplateModal.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class AddTemplateModal extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
---
+++ src/frontend/components/textTemplates/templateList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class TemplateList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+  render() {
+    return(
+      <ul>
+
+      </ul>
+    )
+  }
+}
